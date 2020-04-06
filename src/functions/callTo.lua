281dcaecb9177f9e390b55a3f3a02d472b57806f
Noah Crowley <noahwillcrow@gmail.com>
2020-04-06 19:42:25 -0400

    clean up callTo

--- src/functions/callTo.lua
+++ src/functions/callTo.lua
@@ -5,7 +5,7 @@ function isNaN(val)
 	return val ~= val
 end
 
-function varargsToTable(...)
+function varArgsToTable(...)
 	return { length = select("#", ...), ... }
 end
 
@@ -14,10 +14,10 @@ function doesVarArgsTableMatchExpectations(actual, expected)
 		return false
 	end
 
-	for j = 1, actual.length do
-		local doesJthArgMatch = expected[j] == wildcard or
-			actual[j] == expected[j] or
-			(isNaN(actual[j]) and isNaN(expected[j]))
+	for i = 1, actual.length do
+		local doesJthArgMatch = expected[i] == wildcard or
+			actual[i] == expected[i] or
+			(isNaN(actual[i]) and isNaN(expected[i]))
 
 		if not doesJthArgMatch then
 			return false
@@ -28,15 +28,16 @@ function doesVarArgsTableMatchExpectations(actual, expected)
 end
 
 return function (fakedTable, key, ...)
-	local expectedArgs = varargsToTable(...)
+	local expectedArgs = varArgsToTable(...)
 
-	if not fakedTable[key] then
-		fakedTable[key] = {
+	if not rawget(fakedTable, key) then
+		rawset(fakedTable, key, setmetatable({
 			[internalsSymbol] = {
 				callHistory = {}
-			},
+			}
+		}, {
 			__call = function(tbl, ...)
-				local givenArgs = varargsToTable(...)
+				local givenArgs = varArgsToTable(...)
 
 				table.insert(tbl[internalsSymbol].callHistory, givenArgs)
 
@@ -47,7 +48,7 @@ return function (fakedTable, key, ...)
 
 				local returnValueGetter = nil
 
-				for i = 1, returnsArray.length do
+				for i = 1, #returnsArray do
 					local returnInfo = returnsArray[i]
 					if doesVarArgsTableMatchExpectations(givenArgs, returnInfo.args) then
 						returnValueGetter = returnInfo.valueGetter
@@ -61,7 +62,7 @@ return function (fakedTable, key, ...)
 					return nil
 				end
 			end
-		}
+		}))
 	end
 
 	return {
@@ -88,7 +89,7 @@ return function (fakedTable, key, ...)
 			table.insert(fakedTable[internalsSymbol].functionReturns[key], {
 				args = expectedArgs,
 				valueGetter = function()
-					return table.unpack(returnVals)
+					return unpack(returnVals)
 				end
 			})
 		end
