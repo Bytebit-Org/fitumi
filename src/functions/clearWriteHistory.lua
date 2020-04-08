d3dbd254ea389170bc3e92dc1e25c4ad623616be
Noah Crowley <noahwillcrow@gmail.com>
2020-04-08 14:09:52 -0400

    Change clearWriteHistory to clear by optional key or clear all

--- src/functions/clearWriteHistory.lua
+++ src/functions/clearWriteHistory.lua
@@ -1,8 +1,8 @@
 local internalsSymbol = require(script.Parent.Parent.internal.internalsSymbol)
 local isFakedTable = require(script.Parent.Parent.internal.isFakedTable)
 
-return function (fakedTable)
+return function (fakedTable, key)
 	assert(isFakedTable(fakedTable), "Can only clear write history on a faked table")
 
-	fakedTable[internalsSymbol].writeHistory = {}
+	fakedTable[internalsSymbol].writeHistory[key] = nil
 end
