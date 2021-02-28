cb739105f0b9680fa3a35519b3c578d76276ede6
Noah Crowley <noahwillcrow@gmail.com>
2021-02-28 11:49:20 -0800

    add new features and tests

--- tests/src/functions/callTo.spec.lua
+++ tests/src/functions/callTo.spec.lua
@@ -1,4 +1,5 @@
-return function()
+return function ()
+    local HttpService = game:GetService("HttpService")
     local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 	local callTo = require(ReplicatedStorage:WaitForChild("fitumi"):WaitForChild("functions"):WaitForChild("callTo"))
@@ -8,8 +9,8 @@ return function()
 		return { length = select("#", ...), ... }
 	end
 
-	describe("callTo", function()
-		it("should return the expected set of methods", function()
+	describe("callTo", function ()
+		it("should return the expected set of methods", function ()
 			local fakedTable = {
 				[internalsSymbol] = {}
 			}
@@ -17,42 +18,64 @@ return function()
 			local result = callTo(fakedTable)
 			expect(result).to.be.ok()
 			expect(result).to.be.a("table")
+			expect(result["executes"]).to.be.a("function")
 			expect(result["didHappen"]).to.be.a("function")
 			expect(result["didNotHappen"]).to.be.a("function")
+			expect(result["throws"]).to.be.a("function")
 			expect(result["returns"]).to.be.a("function")
 		end)
 
-		it("should error when given an invalid input", function()
-			expect(function()
+		it("should error when given an invalid input", function ()
+			expect(function ()
 				callTo(nil)
 			end).to.throw()
-			expect(function()
+			expect(function ()
 				callTo(false)
 			end).to.throw()
-			expect(function()
+			expect(function ()
 				callTo(true)
 			end).to.throw()
-			expect(function()
+			expect(function ()
 				callTo(0)
 			end).to.throw()
-			expect(function()
+			expect(function ()
 				callTo(1)
 			end).to.throw()
-			expect(function()
+			expect(function ()
 				callTo(math.huge)
 			end).to.throw()
-			expect(function()
+			expect(function ()
 				callTo("hello, world")
 			end).to.throw()
-			expect(function()
-				callTo(function() end)
+			expect(function ()
+				callTo(function () end)
 			end).to.throw()
-			expect(function()
+			expect(function ()
 				callTo({})
 			end).to.throw()
 		end)
 
-		it("should report that a call happened correctly", function()
+		it("should execute callbacks as expected", function ()
+			local executionCallbacks = {}
+			local fakedTable = {
+				[internalsSymbol] = {
+					executionCallbacks = executionCallbacks
+				}
+			}
+
+			local executionCount = 0
+			callTo(fakedTable, true):executes(function ()
+				executionCount = executionCount + 1
+			end)
+
+			fakedTable(true)
+			expect(executionCount).to.equal(1)
+
+			fakedTable(false)
+			expect(executionCount).to.equal(1)
+		end)
+
+		it("should report that a call happened correctly", function ()
 			local fakedTable = {
 				[internalsSymbol] = {
 					callHistory = {
@@ -67,7 +90,7 @@ return function()
 			expect(callTo(fakedTable, true, 2, "three"):didHappen()).to.equal(true)
 		end)
 
-		it("should report that a call did not happen correctly", function()
+		it("should report that a call did not happen correctly", function ()
 			local fakedTable = {
 				[internalsSymbol] = {
 					callHistory = {
@@ -82,7 +105,21 @@ return function()
 			expect(callTo(fakedTable, true, 2, "three"):didNotHappen()).to.equal(false)
 		end)
 
-		it("should set single return values as expected", function()
+		it("should enforce throws as expected", function ()
+			local callErrors = {}
+			local errorMessage = HttpService:GenerateGUID()
+			local fakedTable = {
+				[internalsSymbol] = {
+					callErrors = callErrors
+				}
+			}
+
+			callTo(fakedTable):throws(errorMessage)
+
+			expect(fakedTable).to.throw(errorMessage)
+		end)
+
+		it("should enforce return values as expected", function ()
 			local functionReturns = {}
 			local returnValue = {}
 			local fakedTable = {
@@ -93,21 +130,11 @@ return function()
 
 			callTo(fakedTable, true, 2, "three"):returns(returnValue)
 
-			expect(fakedTable[internalsSymbol].functionReturns).to.equal(functionReturns)
-			expect(#functionReturns).to.equal(1)
-			expect(functionReturns[1]).to.be.ok()
-			expect(functionReturns[1]).to.be.a("table")
-			expect(functionReturns[1].args).to.be.ok()
-			expect(functionReturns[1].args).to.be.a("table")
-			expect(functionReturns[1].args[1]).to.be.equal(true)
-			expect(functionReturns[1].args[2]).to.be.equal(2)
-			expect(functionReturns[1].args[3]).to.be.equal("three")
-			expect(functionReturns[1].valueGetter).to.be.ok()
-			expect(functionReturns[1].valueGetter).to.be.a("function")
-			expect(functionReturns[1].valueGetter()).to.be.equal(returnValue)
+			expect(fakedTable(true, 2, "three")).to.equal(returnValue)
+			expect(fakedTable(false, 0, "no")).never.to.be.ok()
 		end)
 
-		it("should set tuple return values as expected", function()
+		it("should return tuples as expected", function ()
 			local functionReturns = {}
 			local fakedTable = {
 				[internalsSymbol] = {
@@ -115,12 +142,14 @@ return function()
 				}
 			}
 
-			callTo(fakedTable):returns(true, 2, "three")
+			callTo(fakedTable, true):returns(1, "two")
 
-			local valuesGetterResult1, valuesGetterResult2, valuesGetterResult3 = functionReturns[1].valueGetter()
-			expect(valuesGetterResult1).to.equal(true)
-			expect(valuesGetterResult2).to.equal(2)
-			expect(valuesGetterResult3).to.equal("three")
+			local correctArgsReturnValues = table.pack(fakedTable(true))
+			expect(#correctArgsReturnValues).to.equal(2)
+			expect(correctArgsReturnValues[1]).to.equal(1)
+			expect(correctArgsReturnValues[2]).to.equal("two")
+
+			expect(fakedTable(false)).never.to.be.ok()
 		end)
 	end)
 end
