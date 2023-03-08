6f9ee3cbc182361e3667d8fb33e6f4de28602808
Noah Crowley <17803348+noahwillcrow@users.noreply.github.com>
2023-03-07 21:59:32 -0500

    Improve callTo type

--- src/functions/callTo.d.ts
+++ src/functions/callTo.d.ts
@@ -1,3 +1,3 @@
 import { CallMatchOptions } from "../classes/CallMatchOptions";
 
-export type callTo = <T = unknown>(fakedTable: object, ...args: T extends Callback ? Parameters<T> : ReadonlyArray<unknown>) => CallMatchOptions<T>;
+export type callTo = <T = unknown>(fakedTable: T, ...args: T extends Callback ? Parameters<T> : ReadonlyArray<unknown>) => CallMatchOptions<T>;
